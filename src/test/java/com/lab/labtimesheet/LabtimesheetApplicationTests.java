4cebf249fc6da52f2e2d6f7abe5481732bcdd248
lamtranthai12345 <tranthailam2001@gmail.com>
2026-08-14 04:15:52 +0700

    project init

---
+++ src/test/java/com/lab/labtimesheet/LabtimesheetApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lab.labtimesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class LabtimesheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
