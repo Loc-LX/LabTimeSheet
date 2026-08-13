f798a138eb5387274d2c73bd510cdac253ffcc58
anhndhe151333 <dookudumb@gmail.com>
2026-08-14 04:15:52 +0700

    project init

---
+++ src/main/java/com/lab/labtimesheet/LabtimesheetApplication.java
@@ -0,0 +1,13 @@
+package com.lab.labtimesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabtimesheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabtimesheetApplication.class, args);
+    }
+
+}
